9bac183f35e02c010c168785c3d51c6c7fcb8284
Geomty <69478549+Geomty@users.noreply.github.com>
2020-12-02 18:01:52 -0500

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+TITLE Installing mineflayer...
+ECHO Please follow the steps below to install mineflayer. If you want to skip a step because what's already provided is correct, just hit enter. If you don't know what to put for a step, you can skip it as well.
+PAUSE
+npm init
+ECHO Cool! You're almost done! The actual mineflayer installation process will begin now. This shouldn't take long.
+PAUSE
+npm install mineflayer
+ECHO You're all set to go! This window will now close. Just a reminder to delete this file or not open it again.
+PAUSE
